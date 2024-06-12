ff01012ab12a809bd83c680b1b7b3c521127c1e3
Rodrigo Turpie <jsepulveda_turpie@hotmail.com>
2024-06-12 09:08:19 -0400

    feat:Implementación objetos de Error y mensajes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
-
+const {HttpStatus,HttpResponseMessage} = require("./enums/httpError")
 
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -27,7 +27,7 @@ app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
+  return res.status(HttpStatus.NOT_FOUND).send({message: HttpResponseMessage.NOT_FOUND});
 });
 
 app.listen(PORT, () => {
